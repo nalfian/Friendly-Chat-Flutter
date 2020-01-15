1c6bf72b884a8ac82bf5cbec8e6a268b5c5b0615
Nur Alfian Julianda <nur.julianda@DDC.LOCAL>
2020-01-15 12:52:25 +0700

    create main userinterface

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(FriendlyChatApp());
+}
+
+class FriendlyChatApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Friendlyc Chat",
+      home: ChatScreen(),
+    );
+  }
+}
+
+class ChatScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Friendly Chat"),
+      ),
+    );
+  }
+}
